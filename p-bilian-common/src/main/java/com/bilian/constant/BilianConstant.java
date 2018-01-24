c9b11a5af6b69e571e2cc09f57bf129a47e7e335
xu.xiaoming535 <xu.xiaoming535@chinaredstar.com>
2018-01-24 11:25:14 +0800

    添加常亮interface

---
+++ p-bilian-common/src/main/java/com/bilian/constant/BilianConstant.java
@@ -0,0 +1,16 @@
+package com.bilian.constant;
+
+/**
+ * <p/>
+ * <p><b>introduction:</b>
+ * </p>
+ * <p><b>packageName:</b>com.bilian.constant</p>
+ * <p><b>projectName:</b>p-bilian-parent</p>
+ * <p><b>User:</b> xuxiaoming <a href="mailto:xu.xiaoming535@chinaredstar.com">xu.xiaoming535@chinaredstar.com</a></p>
+ * <p><b>Date:</b>2018/1/24</p>
+ *
+ * @author xuxiaoming
+ */
+public interface BilianConstant {
+    public final String ONE = "1";
+}
